0f8ad317f54053613ad589411d298a75a1c51825
janamirelly <janaynamirelly@gmail.com>
2025-06-03 20:31:51 -0300

    Projeto concluído

---
+++ index.js
@@ -0,0 +1,18 @@
+import { criarItemDaLista } from "./script/criarItemDaLista.js";
+import verificarListaVazia from "./script/verificarListaVazia.js";
+
+const listaDeCompras = document.getElementById("lista-de-compras");
+
+const botaoAdicionar = document.getElementById("adicionar-item");
+
+botaoAdicionar.addEventListener("click", (evento) => {
+  evento.preventDefault();
+  const itemDaLista = criarItemDaLista();
+  listaDeCompras.appendChild(itemDaLista);
+
+  verificarListaVazia(listaDeCompras);
+});
+
+
+
+verificarListaVazia(listaDeCompras);
